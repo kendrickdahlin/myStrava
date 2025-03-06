1bd31cb7e4e04d4c8a7570092f82144a815fdb0a
Kendrick Dahlin <kendrickdahlin@gmail.com>
2025-03-06 12:35:11 +0100

    removed configs

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,2 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export', // Enables static export
-  images: { unoptimized: true }, // Fixes image issues
-  basePath: "/myStrava", // Set this to your GitHub repo name
-};
-
-module.exports = nextConfig;
